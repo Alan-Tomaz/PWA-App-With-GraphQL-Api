8e859ea6ca181e77f7e64de196f3429b233fcbb4
Alan <alan4tomaz8@gmail.com>
2025-12-21 19:38:54 -0300

    Ajustado build do next.js

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,7 @@ const NODE_ENV = process.env.NODE_ENV || "development";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: "export",
 };
 
 const withPWA = require("next-pwa")({
