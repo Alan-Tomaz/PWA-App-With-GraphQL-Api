d1fb58fa615488a017cec861fa468513e466d346
Alan <alan4tomaz8@gmail.com>
2025-12-21 19:45:52 -0300

    Ajustado build do next.js

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,18 @@
 import type { NextConfig } from "next";
 const NODE_ENV = process.env.NODE_ENV || "development";
 
+const repo =
+  process.env.GITHUB_REPOSITORY?.split("/")[1] ||
+  process.env.NEXT_PUBLIC_REPO ||
+  "";
+
+const isGithubPages = !!repo;
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: "export",
+  basePath: isGithubPages ? `/${repo}` : "",
+  assetPrefix: isGithubPages ? `/${repo}` : "",
 };
 
 const withPWA = require("next-pwa")({
