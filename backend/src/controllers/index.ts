db7d2caabd69c3d34bd051d11c8065d5cf23f89b
Alan <alan4tomaz8@gmail.com>
2025-12-25 12:44:43 -0300

    Configurado Node Cache

--- backend/src/controllers/index.ts
+++ backend/src/controllers/index.ts
@@ -1,9 +1,22 @@
 import { Request, Response } from "express";
 import { prisma } from "../config/prismaClient.js";
+import { cache } from "../config/nodeCache.js";
 
 export const getNotifications = async (req: Request, res: Response) => {
   /* GET ALL NOTIFICATIONS */
-  const notifications = await prisma.notification.findMany();
+  let data;
 
-  res.status(200).json(notifications);
+  /* IF HAS NOTIFICATION IN CACHE, RETURN IT. CASE NOT, SEARCH THE NOTIFICATIONS IN DATABASE */
+  const cached = cache.get("notifications");
+  if (cached) {
+    console.log("📦 Cache hit");
+    data = cached;
+  } else {
+    console.log("🗄️ Cache miss");
+    const notifications = await prisma.notification.findMany();
+    data = notifications;
+    cache.set("notifications", notifications);
+  }
+
+  res.status(200).json(data);
 };
